a909bad05ef6951b13d96d5c7284d5ba7ab93e45
hanoopy <user@USER-no-MacBook-Air.local>
2014-10-29 18:04:03 +0900

    Initial Commit

---
+++ PinchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinchApp
+//
+//  Created by yukichi on 2014/10/29.
+//  Copyright (c) 2014年 hanoopy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
